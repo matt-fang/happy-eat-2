b12611931b8d3d020a7fa515e7dc1c2db4b86aed
matt-fang <matthewfang.calif@gmail.com>
2025-02-04 14:45:52 -0800

    Initial Commit

---
+++ new-eat-appTests/new_eat_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  new_eat_appTests.swift
+//  new-eat-appTests
+//
+//  Created by Matthew Fang on 2/4/25.
+//
+
+import Testing
+@testable import new_eat_app
+
+struct new_eat_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
