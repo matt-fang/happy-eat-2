3838e1459fd13c59bab003051674af6833356239
matt-fang <matthewfang.calif@gmail.com>
2025-02-06 23:27:06 -0800

    got rid of unused duplicate presets

--- new-eat-app/Models/Habit.swift
+++ new-eat-app/Models/Habit.swift
@@ -21,24 +21,4 @@ struct Habit: Identifiable {
     
     var pinnedEntry: Entry?
     
-    static let habitPresets: [Habit] = [
-        Habit(name: "Eat more veggies", totalGoalCount: 3),
-        Habit(name: "Eat more fruits", totalGoalCount: 3),
-        Habit(name: "Eat more protein", totalGoalCount: 3),
-        Habit(name: "Eat more whole foods", totalGoalCount: 4),
-        Habit(name: "Eat without distraction", totalGoalCount: 3),
-        Habit(name: "Eat meals on time", totalGoalCount: 3),
-        Habit(name: "Mindfully savor food", totalGoalCount: 3),
-        Habit(name: "Eat without guilt", totalGoalCount: 3),
-        Habit(name: "Cook my own food!", totalGoalCount: 1)
-    ]
-    
-    static let whyPresets: [String] = [
-        "Edit this later!",
-        "To physically feel better",
-        "To feel more confident",
-        "To build consistency",
-        "To improve my relationship with food",
-        "To achieve a fitness goal"
-    ]
 }
